3dece86a836f97e8b4a379d4af99f92be5c8b0d6
chintath <thirupathi.chintala@globalpay.com>
2020-12-11 15:50:37 +0530

    Task Completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormArray, FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'task';
+
+  data = new FormArray([
+    new FormGroup({ title: new FormControl('test 1'), member: new FormControl('test 2') }),
+    new FormGroup({ title: new FormControl('test title 2'), member: new FormControl('test member 2') }),
+  ]);
+
+  addItem() {
+    const group = new FormGroup({
+      title: new FormControl(''),
+      member: new FormControl('')
+    });
+    this.data.push(group);
+  }
+
+  removeItem(index) {
+    this.data.removeAt(index);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+
 
 @NgModule({
   declarations: [
@@ -10,7 +17,12 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    FormsModule, ReactiveFormsModule,
+    MatIconModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
